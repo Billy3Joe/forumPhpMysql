89e7339ead5847d4dccb107dc560714959cb4b87
Billy Joe KOM KAMZE <billykamze3@gmail.com>
2022-08-17 13:47:42 +0200

    Publication de la question terminée

--- actions/questions/myQuestionsAction.php
+++ actions/questions/myQuestionsAction.php
@@ -1,1 +1,6 @@
 <?php require 'actions/database.php';?>
+<?php 
+//Requête pour afficher tous les questions par ordre décroissant
+$getAllMyQuestions = $bdd->prepare('SELECT id, titre, description FROM questions WHERE id_auteur = ? ORDER BY id DESC');
+$getAllMyQuestions->execute(array($_SESSION['id']));
+?>
--- my-questions.php
+++ my-questions.php
@@ -1,7 +1,33 @@
+<?php 
+  require('actions/questions/myQuestionsAction.php');
+  require('actions/users/securityAction.php');
+?>
 <!DOCTYPE html>
 <html lang="en">
   <?php include 'includes/head.php'; ?>
 <body>
   <?php include 'includes/navbar.php'; ?>
   <br><br>
+  <div class="container">
+    <?php 
+          //On affiche tous les questions
+          while($question = $getAllMyQuestions->fetch()){
+      ?> 
+     
+        <div class="card">
+            <div class="card-header">    
+               <?php echo $question['titre']; ?>
+            </div>
+            <div class="card-body">
+                <p class="card-text"><?php echo $question['description']; ?></p>
+                <a href="#" class="btn btn-primary">Accéder à la question</a>
+                <a href="#" class="btn btn-warning">Modifier lla question</a>
+            </div>
+          </div>
+          <br>
+      <?php 
+        }
+      ?>
+   </div>
+  <body>
 </html>
