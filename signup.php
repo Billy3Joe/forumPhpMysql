b84cd3f89923d781ca149df982e2f94bc9dc208f
Billy Joe KOM KAMZE <billykamze3@gmail.com>
2022-08-20 03:29:27 +0200

    Retouche du login

--- signup.php
+++ signup.php
@@ -12,26 +12,26 @@ if (isset($errorMsg)) {
 <body>
     <br><br>
     <form class="container" method="POST">
-    <div class="mb-3">
-        <label for="exampleInputEmail1" class="form-label">Pseudo</label>
-        <input type="text" class="form-control" name="pseudo">
-    </div>
-    <div class="mb-3">
-        <label for="exampleInputEmail1" class="form-label">Nom</label>
-        <input type="text" class="form-control" name="lastname">
-    </div>
-    <div class="mb-3">
-        <label for="exampleInputEmail1" class="form-label">Prenom</label>
-        <input type="text" class="form-control" name="firstname">
-    </div>
-    <div class="mb-3">
-        <label for="exampleInputPassword1" class="form-label">Password</label>
-        <input type="password" class="form-control" name="password">
-    </div>
-    <button type="submit" class="btn btn-primary" name="validate">S'inscrire</button>
-    <button type="reset" class="btn btn-primary">Réinitialiser</button>
-    <br><br>
-    <a href="login.php"><p>J'ai déjà un compte , je me connecte</p></a>
+        <div class="mb-3">
+            <label for="exampleInputEmail1" class="form-label">Pseudo</label>
+            <input type="text" class="form-control" name="pseudo">
+        </div>
+        <div class="mb-3">
+            <label for="exampleInputEmail1" class="form-label">Nom</label>
+            <input type="text" class="form-control" name="lastname">
+        </div>
+        <div class="mb-3">
+            <label for="exampleInputEmail1" class="form-label">Prenom</label>
+            <input type="text" class="form-control" name="firstname">
+        </div>
+        <div class="mb-3">
+            <label for="exampleInputPassword1" class="form-label">Password</label>
+            <input type="password" class="form-control" name="password">
+        </div>
+        <button type="submit" class="btn btn-primary" name="validate">S'inscrire</button>
+        <button type="reset" class="btn btn-primary">Réinitialiser</button>
+        <br><br>
+        <a href="login.php"><p>J'ai déjà un compte , je me connecte</p></a>
     </form>
 </body>
 </html>
